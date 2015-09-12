18d4aeb4ab544a91e34ac6b73136855b6876616b
Eric Bailey <eric@ericb.me>
2015-09-12 13:01:37 -0500

    Add hiccup and generate-message-view

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies     [[org.clojure/clojure "1.7.0"]
                      [compojure "1.4.0"]
-                     [ring/ring-defaults "0.1.5"]]
+                     [ring/ring-defaults "0.1.5"]
+                     [hiccup "1.0.5"]]
   :plugins          [[lein-ring "0.8.13"]]
   :ring             {:handler chatter.handler/app}
   :profiles         {:dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,20 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [hiccup.page :refer [html5]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn generate-message-view
+  "Generate the HTML for displaying messages."
+  []
+  (html5
+   [:head
+    [:title "Chatter"]]
+   [:body
+    [:h1 "Our Chat App"]]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (generate-message-view))
   (route/not-found "Not Found"))
 
 (def app
